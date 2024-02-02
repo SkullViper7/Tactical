b0de5e20c14eb129b5ee0e7e1a44f35fe49d9634
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-02-02 12:02:57 +0100

    grid manager 1st test

---
+++ Assets/Scripts/Grid/GridStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridStat : MonoBehaviour
+{
+    public int Visited = -1;
+
+    public int X = 0;
+    public int Y = 0;
+}
