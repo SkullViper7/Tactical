ac7dbd441328c29deb8658f20dac3bdebfc79faa
Maxime HARRY <maxime.harry@e-artsup.net>
2024-02-02 11:09:15 +0100

    Analyzer Installed

---
+++ Assets/Scripts/Humans/ScriptableJob.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Job_Name", menuName = "Player/New_Job")]
+public class ScriptableJob : ScriptableObject
+{
+    public int Atk;
+
+}
