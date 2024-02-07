0a094ae6f70fc32fdeccbe63cda6e172c8c31fe0
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-02-07 20:17:37 +0100

    highlight

---
+++ Assets/Scripts/Grid/HighlightGrid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightGrid : MonoBehaviour
+{
+    [SerializeField]
+    Material _baseMat;
+    [SerializeField]
+    Material _highlightMat;
+
+    GameObject _targetGrid;
+
+    PathFinding _pathFindingScript;
+
+    List<PathNode> _path;
+
+    [SerializeField]
+    GridObject _targetCharacter;
+
+    private void Start()
+    {
+        _targetGrid = GameObject.Find("Grid");
+        _pathFindingScript = _targetGrid.GetComponent<PathFinding>();
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<MeshRenderer>().material = _highlightMat;
+        _path = _pathFindingScript.FindPath(_targetCharacter.PositionOnGrid.x, _targetCharacter.PositionOnGrid.y, gridPosition.x, gridPosition.y);
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<MeshRenderer>().material = _baseMat;
+    }
+}
