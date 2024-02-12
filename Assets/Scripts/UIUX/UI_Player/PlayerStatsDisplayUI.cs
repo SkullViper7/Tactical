b8eb1f58720bf242181cdfa39a59930631d7904c
NayLiiac <levoyer.kylian24@gmail.com>
2024-02-12 13:40:18 +0100

    add script : PlayerStatsDisplayOnUI

--- Assets/Scripts/UIUX/UI_Player/PlayerStatsDisplayUI.cs
+++ Assets/Scripts/UIUX/UI_Player/PlayerStatsDisplayUI.cs
@@ -1,11 +1,27 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatsDisplayUI : MonoBehaviour
 {
-    [SerializeField]
-    TextMeshProUGUI _playerName;
-    TextMeshProUGUI _playerMP;
-    TextMeshProUGUI _playerAP;
+    [SerializeField] TextMeshProUGUI _playerName;
+    public Slider PlayerHealth;
+    public TextMeshProUGUI PlayerMP;
+    public TextMeshProUGUI PlayerAP;
 
+    public void UpdatePlayerStatsUI(Human human)
+    {
+        // Set up Name
+        _playerName.text = human.name;
+
+        // Set up HP
+        PlayerHealth.maxValue = human.HP;
+        PlayerHealth.value = human.CurrentHP;
+
+        // Set up MP
+        PlayerMP.text = $"{human.CurrentMP} / {human.MP}";
+
+        // Set up AP
+        PlayerAP.text = $"{human.CurrentAP} / {human.CurrentAP}";
+    }
 }
