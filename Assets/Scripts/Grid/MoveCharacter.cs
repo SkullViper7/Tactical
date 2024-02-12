b9f42077db5d7a16220814df9d7dd58f91467100
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-02-12 10:29:32 +0100

    click fix

--- Assets/Scripts/Grid/MoveCharacter.cs
+++ Assets/Scripts/Grid/MoveCharacter.cs
@@ -10,6 +10,9 @@ public class MoveCharacter : MonoBehaviour
 
     HighlightPath _highlightPath;
 
+    [SerializeField]
+    LayerMask _terrainLayer;
+
     /// <summary>
     /// Registering the OnClick function to the input system.
     /// </summary>
@@ -30,13 +33,21 @@ public class MoveCharacter : MonoBehaviour
         // Check if the context for the input started
         if (context.started)
         {
-            // If the highlight path component indicates that movement is possible
-            if (_highlightPath.CanMove)
+            // Create a ray from the main camera to the current mouse position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Cast a ray from the camera to find the objects hit by the ray within a maximum distance and with a the Terrain layer mask
+            if (Physics.Raycast(ray, out hit, float.MaxValue, _terrainLayer))
             {
-                // Move the target character using the PlayerMovement component and the highlighted path
-                _targetCharacter.GetComponent<PlayerMovement>().Move(_highlightPath.Path);
-                // Disable the highlights after the movement
-                _highlightPath.DisableHighights();
+                // If the highlight path component indicates that movement is possible
+                if (_highlightPath.CanMove)
+                {
+                    // Move the target character using the PlayerMovement component and the highlighted path
+                    _targetCharacter.GetComponent<PlayerMovement>().Move(_highlightPath.Path);
+                    // Disable the highlights after the movement
+                    _highlightPath.DisableHighights();
+                }
             }
         }
     }
