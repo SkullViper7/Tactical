d6faed1eb018a083118ebc441ff2098af0a66e04
Bryan Berge <berge.bryan@gmail.com>
2024-02-12 10:23:14 +0100

    Monster movements + attack

--- Assets/Scripts/Monsters/MonsterAttack.cs
+++ Assets/Scripts/Monsters/MonsterAttack.cs
@@ -49,13 +49,13 @@ public class MonsterAttack : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        Debug.Log(_monstersMovements.CanAttack);
-        if (_monstersMovements.CanAttack)
-        {
-            _human = _monstersMovements._human.GetComponent<Human>();
-            UseAttack(_monsters, _human);
-        }
-    }
+    //private void Update()
+    //{
+    //    Debug.Log(_monstersMovements.CanAttack);
+    //    if (_monstersMovements.CanAttack)
+    //    {
+    //        _human = _monstersMovements._human.GetComponent<Human>();
+    //        UseAttack(_monsters, _human);
+    //    }
+    //}
 }
--- Assets/Scripts/Monsters/MonstersMovements.cs
+++ Assets/Scripts/Monsters/MonstersMovements.cs
@@ -18,6 +18,7 @@ public class MonstersMovements : MonoBehaviour
     private PathFinding _pathFindingScript;
 
     private Monsters _monsters;
+    private MonsterAttack _monsterAttack;
 
     public bool CanAttack = false;
     public bool CanMove = true;
@@ -28,30 +29,19 @@ public class MonstersMovements : MonoBehaviour
         _gridObjectMonster = GetComponent<GridObject>();
         _pathFindingScript = _targetGrid.GetComponent<PathFinding>();
         _monsters = GetComponent<Monsters>();
+        _monsterAttack = GetComponent<MonsterAttack>();
     }
 
     public void Move(List<PathNode> path)
     {
+        CanMove = true;
+
         _pathWorldPositions = _gridObjectMonster.TargetGrid.ConvertPathNodesToWorldPositions(path);
 
         _gridObjectMonster.PositionOnGrid.x = path[path.Count - 1].Pos_X;
         _gridObjectMonster.PositionOnGrid.y = path[path.Count - 1].Pos_Y;
-    }
 
-    public void FindPath()
-    {
-        CanMove = true;
-
-        _path = _pathFindingScript.FindPath(_gridObjectMonster.PositionOnGrid.x, _gridObjectMonster.PositionOnGrid.y, _gridObjectPlayer.PositionOnGrid.x, _gridObjectPlayer.PositionOnGrid.y);
-
-        _path.RemoveAt(_path.Count - 1);
-
-        if (_path == null || _path.Count == 0)
-        {
-               return;
-        }
-
-        Move(_path);
+        TravellingMonster();
     }
 
     public void TravellingMonster()
@@ -60,10 +50,11 @@ public class MonstersMovements : MonoBehaviour
         {
             if (_pathWorldPositions.Count == 0)
             {
-                CanAttack = true;
                 CanMove = false;
                 return;
             }
+
+            transform.position = Vector3.MoveTowards(transform.position, _pathWorldPositions[0], _moveSpeed * Time.deltaTime);
 
             if (Vector3.Distance(transform.position, _pathWorldPositions[0]) < 0.05f)
             {
@@ -72,9 +63,11 @@ public class MonstersMovements : MonoBehaviour
                 CanAttack = false;
             }
 
-            Debug.Log(_pathWorldPositions.Count);
-            transform.position = Vector3.MoveTowards(transform.position, _pathWorldPositions[0], _moveSpeed * Time.deltaTime);
-            FindPath();
+            if (Vector3.Distance(transform.position, _human.transform.position) < 2f && _monsters.MonsterPA > 0)
+            {
+                _monsterAttack.UseAttack(_monsters, _human);
+            }
+
         }
         else
         {
@@ -86,21 +79,40 @@ public class MonstersMovements : MonoBehaviour
     public void SearchPlayerNearby()
     {
         GameObject[] joueurs = GameObject.FindGameObjectsWithTag("Player");
-        float distanceMinimale = Mathf.Infinity;
+        List<PathNode> _finalPath = new List<PathNode>();
+
 
         foreach (GameObject joueur in joueurs)
         {
-            float distance = Vector3.Distance(transform.position, joueur.transform.position);
+            List<PathNode> _currentPath = new List<PathNode>();
 
-            if (distance < distanceMinimale)
+            GridObject _gridObjectPlayerTempo = joueur.GetComponent<GridObject>();
+            _currentPath = _pathFindingScript.FindPath(_gridObjectMonster.PositionOnGrid.x, _gridObjectMonster.PositionOnGrid.y, _gridObjectPlayerTempo.PositionOnGrid.x, _gridObjectPlayerTempo.PositionOnGrid.y);
+
+            if (_finalPath.Count == 0)
             {
-                distanceMinimale = distance;
+                _finalPath = _currentPath;
+                _human = joueur.GetComponent<Human>();
+                _gridObjectPlayer = joueur.GetComponent<GridObject>();
+            }
+
+            if (_currentPath.Count < _finalPath.Count)
+            {
+                _finalPath = _currentPath;
                 _human = joueur.GetComponent<Human>();
                 _gridObjectPlayer = joueur.GetComponent<GridObject>();
             }
         }
 
-        TravellingMonster();
+        _path = _finalPath;
+        _path.RemoveAt(_path.Count - 1);
+
+        if (_path == null || _path.Count == 0)
+        {
+            return;
+        }
+
+        Move(_path);
     }
 
     private void Update()
