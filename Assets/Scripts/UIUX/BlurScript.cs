198ae52e9588b20b79b6d356fe2cd2d406c6ab3a
Bryan Berge <berge.bryan@gmail.com>
2024-02-15 21:35:19 +0100

    UI Set

---
+++ Assets/Scripts/UIUX/BlurScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BlurScript : MonoBehaviour
+{
+    public void UpdateBlur()
+    {
+        PostProcessVolume _ppVolume = Camera.main.gameObject.GetComponent<PostProcessVolume>();
+        _ppVolume.enabled = !_ppVolume.enabled;
+    }
+}
