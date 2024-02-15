1da9745de816a0f915497e32ceaffbbb3b05f7e7
Bryan Berge <berge.bryan@gmail.com>
2024-02-15 22:51:55 +0100

    Add UI

---
+++ Assets/Scripts/UIUX/IndicatorRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 90f;
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _positionY;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime);
+        transform.position = new Vector3(_target.position.x, _positionY, _target.position.z);
+    }
+}
