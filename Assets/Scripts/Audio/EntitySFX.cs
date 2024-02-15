f7fd69383c7ba3974834c2c5a06943d897fcfbb8
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-02-15 18:04:31 +0100

    enity sfx

---
+++ Assets/Scripts/Audio/EntitySFX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySFX : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> _footsteps;
+
+    [SerializeField]
+    AudioClip _attack;
+
+    [SerializeField]
+    AudioClip _hurt;
+
+    [SerializeField]
+    AudioClip _death;
+
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Step()
+    {
+        int randomIndex = Random.Range(0, _footsteps.Count);
+
+        _audioSource.PlayOneShot(_footsteps[randomIndex]);
+    }
+
+    public void Attack()
+    {
+        _audioSource.PlayOneShot(_attack);
+    }
+
+    public void TakeDamage()
+    {
+        _audioSource.PlayOneShot(_hurt);
+    }
+
+    public void Die()
+    {
+        _audioSource.PlayOneShot(_death);
+    }
+}
